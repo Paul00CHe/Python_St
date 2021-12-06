028eda1f269db6bc11af1b6d90ccd172366ca1fc
DH <revan_01_10@yahoo.com>
2021-12-06 18:40:52 +0300

    Задача 15 решается без формулы.

--- Progs/Project Euler/task 15.py
+++ Progs/Project Euler/task 15.py
@@ -1,17 +1,26 @@
 # Начиная в левом верхнем углу сетки 2×2 и имея возможность двигаться только вниз или вправо,
 # существует ровно 6 маршрутов до правого нижнего угла сетки.
 # Сколько существует таких маршрутов в сетке 20×20?
-
-# используя формулу комбинаторики https://ichi.pro/ru/ponimanie-kombinatoriki-kolicestvo-putej-v-setke-208765624419204
+import copy
 n = 21 # не корректно задано условие (6 маршрутов у сетки 3х3, а не 2х2)
+A = [[i for i in range(m * n, m * n + n)] for m in range(n)]
+print(A)
 
 
-def factorial(x):
-    a = 1
-    for i in range(2, x + 1):
-        a *= i
-    return a
+def opr_puti(x, y):
+    z = copy.deepcopy(x)
+    x.append(x[-1] + 1)
+    z.append(x[-1] + y)
+    return x, z
 
 
-putei = factorial(2 * (n - 1)) / (factorial(n - 1) * factorial(n - 1))
-print(putei)
+#i = 0
+#v = []
+#while True:
+
+    #if i < A[-1]:
+
+
+#b = opr_puti(v, n)[1]
+#print(v)
+#print(b)
