8fcfe6d47b665bd3980f74ee2e867218013165e1
DH <revan_01_10@yahoo.com>
2021-12-24 20:18:37 +0300

    Задача 15. Решена 2-мя способами.

--- Progs/Project Euler/task 15.py
+++ Progs/Project Euler/task 15.py
@@ -3,15 +3,33 @@
 # Сколько существует таких маршрутов в сетке 20×20?
 
 
+# 1-й вариант
 def ways(n):
-    A = []
+    net = [[0 for i in range(m * n, m * n + n)] for m in range(n)]
     for i in range(n):
         for j in range(n):
             if i == 0 or j == 0:
-                A.append(1)
+                net[i][j] = 1
             else:
-                A[i][j] = A[i - 1][j] + A[i][j - 1]
-    return A[n - 1][n - 1]
+                net[i][j] = net[i - 1][j] + net[i][j - 1]
+    return net[len(net) - 1][len(net) - 1]
 
 
-print(ways(20))
+print(ways(200))
+
+# 2-й вариант
+memory = {(1, 0): 1, (0, 1): 1}
+
+
+def func(x, y):
+    if (x, y) not in memory:
+        if x == 0:
+            memory[(x, y)] = func(x, y - 1)
+        elif y == 0:
+            memory[(x, y)] = func(x - 1, y)
+        else:
+            memory[(x, y)] = func(x - 1, y) + func(x, y - 1)
+    return memory[(x, y)]
+
+
+print(func(199, 199))
